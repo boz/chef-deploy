5369371c415b3ea92b467ea3b2a42b2ee7ff9768
Adam Bozanich <adam.boz@gmail.com>
2013-07-24 14:46:35 -0700

    fix rsyslog priority attribute

--- resources/rsyslog.rb
+++ resources/rsyslog.rb
@@ -1,4 +1,4 @@
 actions :install
 default_action :install
 attribute :path    , :kind_of => String          , :required => true
-attribute :priority, :kind_of => [Integer,String], :required => true, :default => 60
+attribute :priority, :kind_of => [Integer,String], :default => "60"
